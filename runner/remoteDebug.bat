c0000d6784d18710abbfaacc16ce3ffce6125ea5
Alejandro Medrano <amedrano@lst.tfo.upm.es>
2013-04-04 16:20:47 +0200

    MW 2.0 Pax Runner configuration.

--- runner/remoteDebug.bat
+++ runner/remoteDebug.bat
@@ -1,1 +1,1 @@
-java -Dfelix.config.properties=file:felix/config.ini -Dfelix.system.properties=file:felix/system.properties -Xdebug -Xnoagent -Djava.compiler=NONE -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=5005 -jar bundles/org.apache.felix.main_*.jar
+java -Dfelix.config.properties=file:felix/config.ini -Dfelix.system.properties=file:felix/system.properties -Xdebug -Xnoagent -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=5015 -jar bundles/org.apache.felix.main_*.jar
