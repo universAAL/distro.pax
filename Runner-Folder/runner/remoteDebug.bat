d674b045b8b9c17daf37b436535e5ac4f633f49f
Carsten Stocklöw <stockloew@gmx.de>
2017-03-15 01:28:59 +0100

    removed the runner folder, moved content of rundir to root

--- Runner-Folder/runner/remoteDebug.bat
+++
@@ -1,1 +0,0 @@
-java -Dfelix.config.properties=file:felix/config.ini -Dfelix.system.properties=file:felix/system.properties -Xdebug -Xnoagent -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=5015 -jar bundles/org.apache.felix.main_*.jar
